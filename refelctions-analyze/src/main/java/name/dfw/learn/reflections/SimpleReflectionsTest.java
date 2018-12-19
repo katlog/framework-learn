39f8e23c88ec806d9c7b3f83f0f2c50f8da75d1f
katlog <1375049265@qq.com>
2018-12-19 14:12:41 +0800

    analyze: reflections init

---
+++ refelctions-analyze/src/main/java/name/dfw/learn/reflections/SimpleReflectionsTest.java
@@ -0,0 +1,29 @@
+package name.dfw.learn.reflections;
+
+import org.reflections.Reflections;
+
+import java.util.Set;
+
+public class SimpleReflectionsTest {
+
+    class A{}
+    class B extends A{}
+    class C extends B{}
+    class D extends A{}
+
+    public @interface Inter {
+    }
+    class AI{}
+    @Inter
+    class BI{}
+    class CI extends BI{}
+
+    public static void main(String[] args) {
+        Reflections reflections = new Reflections();
+
+        Set<Class<? extends A>> subTypes = reflections.getSubTypesOf(A.class);
+        System.out.println("subTypes = " + subTypes);
+        Set<Class<?>> annotated = reflections.getTypesAnnotatedWith(Inter.class);
+        System.out.println("annotated = " + annotated);
+    }
+}
